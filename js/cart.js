4e66b7c1fe0ecf92d458b4767cd6f43db8fdaa04
eboonie <ebun.aribido@gmail.com>
2020-08-19 20:20:22 -0400

    made changes

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,43 @@
 //Modal Section
 
-//Checkout Section (quantity)
+
+//Cart Section (quantity)
+//tomatoCrart section
+
+document.getElementById('tomatoCart').addEventListener('input', function (eventLisChild) {
+    console.log("I am working");
+
+    let figures = eventLisChild.target.value;
+    console.log(figures);
+
+    let quantity = document.getElementById('quantityOutput').innerHTML = figures * 5000;
+})
+
+
+//tatasheCart section 
+document.getElementById('tatasheCart').addEventListener('input', function (eventLisChild) {
+    console.log("I am working");
+
+    let figures = eventLisChild.target.value;
+    console.log(figures);
+
+    let quantity = document.getElementById('tatasheQuantityOutput').innerHTML = figures * 4000;
+})
+
+//OnionCart Section
+
+document.getElementById('onionCart').addEventListener('input', function (eventLisChild) {
+    console.log("I am working");
+
+    let figures = eventLisChild.target.value;
+    console.log(figures);
+
+    let quantity = document.getElementById('').innerHTML = figures * 6000;
+})
 
 //Create Account Section
+
+//final checkout
 
 //Existing Customer Section
 
